f3ac66bb466e600428caa5a82d014e09050e463f
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-09-30 10:44:28 -0600

    add grunt help for debugging

--- js/build-server/build-server.js
+++ js/build-server/build-server.js
@@ -633,20 +633,22 @@ var taskQueue = async.queue( function( task, taskCallback ) {
               exec( 'grunt checkout-shas --buildServer=true --repo=' + simName, PERENNIAL, function() {
                 exec( 'git checkout ' + repos[ simName ].sha, simDir, function() { // checkout the sha for the current sim
                   exec( 'npm install', simDir, function() {
-                    exec( 'grunt build-for-server --brand=phet --locales=' + locales, simDir, function() {
-                      mkVersionDir( function() {
-                        exec( 'cp build/* ' + HTML_SIMS_DIRECTORY + simName + '/' + version + '/', simDir, function() {
-                          writeHtaccess( function() {
-                            createTranslationsXML( simTitleCallback, function() {
-                              notifyServer( function() {
-                                addToRosetta( simTitle, function() {
-                                  //spotScp( function() { TODO: do we need this? grunt deploy-production does a spot deploy already
-                                  exec( 'grunt checkout-master-all', PERENNIAL, function() {
-                                    exec( 'rm -rf ' + buildDir, '.', function() {
-                                      taskCallback();
+                    exec( 'grunt --help', simDir, function() {
+                      exec( 'grunt build-for-server --brand=phet --locales=' + locales, simDir, function() {
+                        mkVersionDir( function() {
+                          exec( 'cp build/* ' + HTML_SIMS_DIRECTORY + simName + '/' + version + '/', simDir, function() {
+                            writeHtaccess( function() {
+                              createTranslationsXML( simTitleCallback, function() {
+                                notifyServer( function() {
+                                  addToRosetta( simTitle, function() {
+                                    //spotScp( function() { TODO: do we need this? grunt deploy-production does a spot deploy already
+                                    exec( 'grunt checkout-master-all', PERENNIAL, function() {
+                                      exec( 'rm -rf ' + buildDir, '.', function() {
+                                        taskCallback();
+                                      } );
                                     } );
+                                    //} );
                                   } );
-                                  //} );
                                 } );
                               } );
                             } );
