c469defc7862f379511b68f3e4cab610394f2a32
Jonathan Olson <jonathan.olson@colorado.edu>
2017-11-28 11:04:37 -0700

    Adding sshExecute

---
+++ js/common/sshExecute.js
@@ -0,0 +1,31 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Executes a command on a remote server.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+/* eslint-env node */
+'use strict';
+
+// modules
+const execute = require( './execute' );
+const winston = require( 'winston' );
+
+/**
+ * Executes a command on a remote server.
+ * @public
+ *
+ * @param {string} username
+ * @param {string} host
+ * @param {string} cmd - The process to execute.
+ * @returns {Promise}
+ */
+module.exports = function( username, host, cmd ) {
+  winston.info( 'running ' + cmd + ' remotely on ' + host );
+
+  return execute( 'ssh', [
+    username + '@' + host,
+    cmd
+  ], '.' );
+};
