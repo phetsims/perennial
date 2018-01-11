94ac57e641e48d75edf2036e5416cfaad3ebbe9c
Jonathan Olson <jonathan.olson@colorado.edu>
2018-01-11 14:33:34 -0700

    Adding rc/production to perennial unit tests

---
+++ js/common/getBranches.js
@@ -0,0 +1,29 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Gets a list of branch names from the origin
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+/* eslint-env node */
+'use strict';
+
+// modules
+const execute = require( './execute' );
+const winston = require( 'winston' );
+
+/**
+ * Gets a list of branch names from the origin
+ * @public
+ *
+ * @param {string} repo - The repository name
+ * @returns {Promise.<Array.<string>>} - Whether it is clean or not
+ * @rejects {ExecuteError}
+ */
+module.exports = async function( repo ) {
+  winston.debug( `retrieving branches from ${repo}` );
+
+  return ( await execute( 'git', [ 'ls-remote' ], `../${repo}` ) ).split( '\n' ).filter( line => line.includes( 'refs/heads/' ) ).map( line => {
+    return line.match( /refs\/heads\/(.*)/ )[ 1 ].trim();
+  } );
+};
--- test/perennialGruntTest.js
+++ test/perennialGruntTest.js
@@ -9,10 +9,30 @@
 'use strict';
 
 const execute = require( '../js/common/execute' );
+const getBranches = require( '../js/common/getBranches' );
 const gruntCommand = require( '../js/common/gruntCommand' );
 const qunit = require( 'qunit' );
 
 qunit.module( 'Perennial grunt tasks' );
+
+async function getLikelyNextBranch( repo, incrementMajor, incrementMinor ) {
+  let currentMajor = 0;
+  let currentMinor = 0;
+  ( await getBranches( repo ) ).filter( branch => /^\d+\.\d+$/.test( branch ) ).forEach( branch => {
+    const [ major, minor ] = branch.split( '.' ).map( str => parseInt( str, 10 ) );
+    if ( major > currentMajor || ( major === currentMajor && minor > currentMinor ) ) {
+      currentMajor = major;
+      currentMinor = minor;
+    }
+  } );
+  if ( incrementMajor ) {
+    currentMajor++;
+  }
+  if ( incrementMinor ) {
+    currentMinor++;
+  }
+  return `${currentMajor}.${currentMinor}`;
+}
 
 qunit.test( 'Checkout target', async ( assert ) => {
   assert.timeout( 120000 );
@@ -33,5 +53,43 @@ qunit.test( 'Bumper dev phet,phet-io', async ( assert ) => {
   await execute( 'git', [ 'checkout', '2.0'], '../chipper' );
   await execute( 'git', [ 'checkout', 'chipper2.0'], '../phet-io-wrappers' );
   await execute( gruntCommand, [ 'dev', '--repo=bumper', '--brands=phet,phet-io', '--noninteractive' ], '.' );
+  assert.expect( 0 );
+} );
+
+qunit.test( 'Bumper dev phet', async ( assert ) => {
+  assert.timeout( 600000 );
+  // TODO: Remove these once it goes to master
+  await execute( 'git', [ 'checkout', '2.0'], '../chipper' );
+  await execute( 'git', [ 'checkout', 'chipper2.0'], '../phet-io-wrappers' );
+  await execute( gruntCommand, [ 'dev', '--repo=bumper', '--brands=phet', '--noninteractive' ], '.' );
+  assert.expect( 0 );
+} );
+
+qunit.test( 'Bumper dev phet-io', async ( assert ) => {
+  assert.timeout( 600000 );
+  // TODO: Remove these once it goes to master
+  await execute( 'git', [ 'checkout', '2.0'], '../chipper' );
+  await execute( 'git', [ 'checkout', 'chipper2.0'], '../phet-io-wrappers' );
+  await execute( gruntCommand, [ 'dev', '--repo=bumper', '--brands=phet-io', '--noninteractive' ], '.' );
+  assert.expect( 0 );
+} );
+
+qunit.test( 'Major bump, RC/Production', async( assert ) => {
+  assert.timeout( 3000000 );
+  // TODO: Remove these once it goes to master
+  await execute( 'git', [ 'checkout', '2.0'], '../chipper' );
+  await execute( 'git', [ 'checkout', 'chipper2.0'], '../phet-io-wrappers' );
+  const branch = getLikelyNextBranch( 'bumper', true, false );
+
+  // We can't create the branch interactively (for maintenance releases), so we do so here
+  await execute( gruntCommand, [ 'create-release', '--repo=bumper', `--branch=${branch}` ], '.' );
+
+  // should be rc.1 and maintenance:0
+  await execute( gruntCommand, [ 'rc', '--repo=bumper', '--brands=phet,phet-io', `--branch=${branch}`, '--noninteractive' ], '.' );
+  await execute( gruntCommand, [ 'production', '--repo=bumper', '--brands=phet,phet-io', `--branch=${branch}`, '--noninteractive' ], '.' );
+
+  // same thing, but maintenance:1
+  await execute( gruntCommand, [ 'rc', '--repo=bumper', '--brands=phet,phet-io', `--branch=${branch}`, '--noninteractive' ], '.' );
+  await execute( gruntCommand, [ 'production', '--repo=bumper', '--brands=phet,phet-io', `--branch=${branch}`, '--noninteractive' ], '.' );
   assert.expect( 0 );
 } );
