6586cae6e1c5f3d75fc84fda5a1aab111dfd7373
mattpen <matthew.pennington@colorado.edu>
2018-03-21 13:56:46 -0600

    Added debugging to writefile

--- js/build-server/writeFile.js
+++ js/build-server/writeFile.js
@@ -4,9 +4,11 @@
 'use strict';
 
 const fs = require( 'graceful-fs' ); // eslint-disable-line
+const winston = require( 'winston' );
 
 module.exports = async function ( filepath, contents ) {
   return new Promise( ( resolve, reject ) => {
+    winston.info( 'Writing file to path: ' + filepath );
     fs.writeFile( filepath, contents, err => {
       if ( err ) {
         reject( err );
