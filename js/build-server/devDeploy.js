4599607fc9ded0d0d13473229323d68f944bc3a7
mattpen <matthew.pennington@colorado.edu>
2018-05-14 13:41:23 -0600

    Added allowance for a11y files in dev deploys #89

--- js/build-server/devDeploy.js
+++ js/build-server/devDeploy.js
@@ -40,7 +40,7 @@ module.exports = async function( simDir, simName, version, chipperVersion, brand
       }
       targetDir += '/.rsync-filter';
 
-      const rsyncFilterContents = '- *_CA*\n+ *_en*\n+ *_all*\n- *.html';
+      const rsyncFilterContents = '- *_CA*\n+ *_en*\n+ *_all*\n+ *_a11y*\n- *.html';
       await writeFile( targetDir, rsyncFilterContents );
     }
     await new Promise( ( resolve, reject ) => {
