c0ff86f7fa031a0ec9257562819e059978a28e51
samreid <reids@colorado.edu>
2018-07-12 10:09:47 -0600

    Added repo-report.js, see https://github.com/phetsims/tasks/issues/942

---
+++ js/scripts/repo-report.js
@@ -0,0 +1,65 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * usage:
+ * cd {{repo}}
+ * node ../perennial/js/scripts/repo-report.js > out.txt
+ * then import in Excel
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ *
+ * TODO: This is a "quick" version which could benefit from documentation, better command line hygiene, more options, etc.
+ * see https://github.com/phetsims/tasks/issues/942
+ */
+const { exec } = require( 'child_process' ); // eslint-disable-line
+
+exec( 'git rev-list master', ( error, stdout, stderr ) => {
+  'use strict';
+  if ( error ) {
+    console.error( `exec error: ${error}` );
+    return;
+  }
+
+  if ( stderr.length === 0 && stdout.length !== 0 ) {
+    const lines = stdout.trim().split( /\n/ ).reverse();
+    console.log( 'sha' + '\t' + 'date' + '\t' + 'LOC' + '\t' + 'TODO' + '\t' + 'REVIEW' );
+    const visit = function( index ) {
+
+      exec( 'git checkout ' + lines[ index ], ( error, stdout, stderr ) => {
+
+        exec( 'grep -ro "TODO" ./js/ | wc -l', ( error, stdout, stderr ) => {
+          const todoCount = stdout.trim();
+
+          exec( 'grep -ro "REVIEW" ./js/ | wc -l', ( error, stdout, stderr ) => {
+            const reviewCount = stdout.trim();
+
+            exec( 'git log -1 --format=format:\'%ai\'', ( error, stdout, stderr ) => {
+              const date = stdout.trim();
+
+              exec( '( find ./js/ -name \'*.js\' -print0 | xargs -0 cat ) | wc -l', ( error, stdout, stderr ) => {
+                const lineCount = stdout.trim();
+
+                // console.log( 'hello ' + lines[ index ] );
+                // console.log( stdout.trim() );
+                // console.log( stdout.trim() );
+                console.log( lines[ index ] + '\t' + date + '\t' + lineCount + '\t' + todoCount + '\t' + reviewCount );
+                if ( index < lines.length - 1 ) {
+                  visit( index + 1 );
+                }
+                else {
+
+                  // done
+                  exec( 'git checkout master', ( error, stdout, stderr ) => {
+                    // console.log( 'checked out master' );
+                  } );
+                }
+              } );
+
+            } );
+          } );
+        } );
+      } );
+    };
+    visit( 0 );
+  }
+} );
