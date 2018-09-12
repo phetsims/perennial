5cd23776dda7fc7e7866ff0635f08661eb7cae80
mattpen <matthew.pennington@colorado.edu>
2018-09-12 14:26:07 -0600

    added 302 redirect for trailing slash

--- js/common/writePhetioHtaccess.js
+++ js/common/writePhetioHtaccess.js
@@ -33,7 +33,9 @@ module.exports = async function writePhetioHtaccess( passwordProtectPath, latest
                                    `RewriteBase /sims/${latestOption.simName}/\n`;
       const versions = JSON.parse( await request( buildLocal.productionServerURL + `/services/metadata/phetio?name=${latestOption.simName}&latest=true` ) );
       for ( let v of versions ) {
-        latestRedirectContents += `RewriteRule ${v.versionMajor}.${v.versionMinor}$ ${v.versionMajor}.${v.versionMinor}.${v.versionMaintenance}${v.versionSuffix ? '-' : ''}${v.versionSuffix}/\n`;
+        // Add a trailing slash to /sims/sim-name/x.y
+        latestRedirectContents += `RewriteRule ${v.versionMajor}.${v.versionMinor}$ ${v.versionMajor}.${v.versionMinor}.${v.versionMaintenance}${v.versionSuffix ? '-' : ''}${v.versionSuffix}/ [R=302]\n`;
+        // Rewrite /sims/sim-name/x.y/* to /sims/sim-name/x.y.z/*
         latestRedirectContents += `RewriteRule ${v.versionMajor}.${v.versionMinor}/(.*) ${v.versionMajor}.${v.versionMinor}.${v.versionMaintenance}${v.versionSuffix ? '-' : ''}${v.versionSuffix}/$1\n`;
       }
       // 'RewriteRule latest(.*) ' + latestOption.version + '$1\n';
