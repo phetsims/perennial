b3d63da49b5149a5fa357301b7f13480abe70975
Matt Pennington <matthew.pennington@colorado.edu>
2020-03-06 15:02:08 -0700

    fixed imports

--- js/build-server/deployImages.js
+++ js/build-server/deployImages.js
@@ -1,5 +1,6 @@
 // Copyright 2020, University of Colorado Boulder
 
+const execute = require( '../common/execute' );
 const gitCheckout = require( '../common/gitCheckout' );
 const gitPull = require( '../common/gitPull' );
 const request = require( 'request' );
