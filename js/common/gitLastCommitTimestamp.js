cdee4dcf24526541dd20eba4b86cf66b6b7b04ab
Jonathan Olson <jonathan.olson@colorado.edu>
2020-04-23 14:39:05 -0600

    Adding gitLastCommitTimestamp

---
+++ js/common/gitLastCommitTimestamp.js
@@ -0,0 +1,27 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Provides the timestamp of the latest commit
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+'use strict';
+
+const execute = require( './execute' );
+const assert = require( 'assert' );
+
+/**
+ * Provides the timestamp of the latest commit
+ * @public
+ *
+ * @param {string} repo - The repository name
+ * @returns {Promise.<number>} - Resolves to the timestamp
+ */
+module.exports = function( repo ) {
+  assert( typeof repo === 'string' );
+
+  return execute( 'git', [ 'log', '-1', '--pretty=format:%cd', '--date=iso' ], `../${repo}` ).then( stdout => {
+    return Promise.resolve( new Date( stdout.trim() ).getTime() );
+  } );
+};
