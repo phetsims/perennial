41087b6be059d4870a1503eff834ffd157a9f03c
pixelzoom <cmalley@pixelzoom.com>
2020-07-06 12:29:06 -0600

    add -p to ignore private repos, #185

--- bin/clone-missing-repos.sh
+++ bin/clone-missing-repos.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 #=============================================================================================
 #
-# Clones all missing repos.
+# Clones all missing repos. Use -p to omit private repos.
 #
 # Author: Aaron Davis
 # Author: Chris Malley (PixelZoom, Inc.)
 #
 #=============================================================================================
 
+omitPrivateRepos="false"
+
+# parse command line options
+while getopts ":p" opt; do
+  case ${opt} in
+    p)
+      omitPrivateRepos="true"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit
+      ;;
+  esac
+done
+
+# location of perennial/bin/
 binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# cd to the directory that contains your git repos
 cd ${binDir}/../..
 
-for repo in `${binDir}/print-missing-repos.sh | xargs`
+if [ ${omitPrivateRepos} == "true" ]; then
+
+  # Identify missing repos, public only
+  missingRepos=`comm -23 <(${binDir}/print-missing-repos.sh) perennial/data/active-repos-private | xargs`
+else
+
+  # Identify missing repos, public and private
+  missingRepos=`${binDir}/print-missing-repos.sh | xargs`
+fi
+
+# Clone missing repos
+for repo in ${missingRepos}
 do
+  echo ${repo}
   git clone https://github.com/phetsims/"${repo}".git
   ( cd ${repo}; git init --template=../phet-info/git-template-dir )
 done
