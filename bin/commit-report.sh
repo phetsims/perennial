6412d2a5cdbe6ab2bc860b1a3f11d5039bea54ff
pixelzoom <cmalley@pixelzoom.com>
2020-07-06 11:09:34 -0600

    #189

--- bin/commit-report.sh
+++ bin/commit-report.sh
@@ -2,18 +2,30 @@
 #=============================================================================================
 #
 # Output a formatted view of recent commits to help in writing a report
-# Usage: commit-report.sh username
+# Usage: commit-report.sh author
 #
 # Author: Sam Reid (PhET Interactive Simulations)
 #
 #=============================================================================================
 
-binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+# Read author from first command line arg
 if [ -z "$1" ]
   then
-    echo "Usage: `basename $0` username"
+    echo "Usage: `basename $0` author"
     exit 1
 fi
+author=$1
 
-${binDir}/for-each.sh active-repos ${binDir}/log.sh $1
+# perennial/bin
+binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# directory that contains your working copies of git repos
+rootDir=${binDir}/../../
+
+for repository in `cat ${rootDir}/perennial/data/active-repos | tr -d '\r' | xargs`
+do
+  echo ${repository}
+  cd ${rootDir}/${repository}
+  git --no-pager log --all --remotes --since=7.days --author=${author} --pretty=format:"%an %ad %s" --date=relative
+  printf "\n\n"
+done
--- bin/log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#=============================================================================================
-# To be used by commit-report.sh only
-# Author: Sam Reid (PhET Interactive Simulations)
-#=============================================================================================
-
-git log --all --remotes --since=7.days --author=$1 --pretty=format:"%an %ad %s" --date=relative
-printf "\n"
-printf "\n"
