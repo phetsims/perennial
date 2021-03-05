b0e8e7ec8852d5be5f6c28cd02e7999a97e93d9d
samreid <reids@colorado.edu>
2021-03-05 09:47:04 -0700

    Add scripts to output and compare macro APIs over time, see https://github.com/phetsims/phet-io/issues/1733

---
+++ js/phet-io/DateUtils.js
@@ -0,0 +1,22 @@
+// Copyright 2021, University of Colorado Boulder
+module.exports = {
+  toGitHubDate( date ) {
+    return date.toISOString().split( 'T' )[ 0 ] + ' 23:59';
+  },
+  toFilename( date ) {
+    return this.toGitHubDate( date ).replace( /:/, '_' );
+  },
+  getTestDates() {
+    const currentDate = new Date();
+
+    const dates = [];
+    for ( let i = 0; i < 10; i++ ) {
+      const dateToOutput = new Date();
+      dateToOutput.setDate( currentDate.getDate() - 10 * i );
+      dates.push( dateToOutput );
+    }
+    console.log( dates.join( '\n' ) );
+
+    return dates;
+  }
+};
