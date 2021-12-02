f7ce5b6b60c92a618e23b85ef446a0732e56874c
Sam Reid <reids@colorado.edu>
2021-12-01 21:21:43 -0700

    Separate type checking from transpilation, remove extraneous output-js tasks, only run type checker during builds, see https://github.com/phetsims/chipper/issues/1156

--- js/common/outputJSAll.js
+++ js/common/outputJSAll.js
@@ -1,7 +1,7 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * Runs `grunt output-js`
+ * Runs `grunt output-js-all`
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  * @author Sam Reid (PhET Interactive Simulations)
@@ -10,20 +10,17 @@
 const ChipperVersion = require( './ChipperVersion' );
 const execute = require( './execute' );
 const gruntCommand = require( './gruntCommand' );
-const loadJSON = require( './loadJSON' );
-const fs = require( 'fs' );
 const winston = require( 'winston' );
 
 /**
  * Outputs JS for a directory
  * @public
  *
- * @param {string} repo
  * @returns {Promise}
  */
-module.exports = async function( repo ) {
+module.exports = async function() {
 
-  winston.info( 'running outputJS' );
+  winston.info( 'running outputJSAll' );
 
   const chipperVersion = ChipperVersion.getFromRepository();
 
@@ -32,18 +29,10 @@ module.exports = async function( repo ) {
   // Not every version of chipper has the output-js task family.  Only proceed if it exists in this version of chipper.
   if ( chipperVersion.chipperSupportsOutputJSGruntTasks ) {
 
-    const packageFilePath = `../${repo}/package.json`;
-    const exists = fs.existsSync( packageFilePath );
-    if ( exists ) {
-      const packageObject = await loadJSON( packageFilePath );
-
-      // Not every repo supports the output-js task, only proceed if it is supported
-      if ( packageObject.phet && packageObject.phet.supportsOutputJS ) {
-        winston.info( 'running grunt output-js' );
-        await execute( gruntCommand, [ 'output-js' ], `../${repo}` );
-        ranOutputJS = true;
-      }
-    }
+    // Not every repo supports the output-js task, only proceed if it is supported
+    winston.info( 'running grunt output-js' );
+    await execute( gruntCommand, [ 'output-js-all' ], '../chipper' );
+    ranOutputJS = true;
   }
   if ( !ranOutputJS ) {
     winston.info( 'outputJS not detected, skipping...' );
