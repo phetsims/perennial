a886e4d2ddae82503a9acb30753079987edfb687
Jonathan Olson <jonathan.olson@colorado.edu>
2022-05-27 11:10:43 -0600

    Adding REVIEW comments for https://github.com/phetsims/perennial/issues/274

--- js/scripts/ts-status.js
+++ js/scripts/ts-status.js
@@ -46,6 +46,9 @@ const completeHeader = '% Complete';
 
 // filter and parse stdout to return lines of code count in each repo
 const formatCodeCount = result => {
+  //REVIEW: This is a lot of complexity to read the output from a shell command
+  //REVIEW: Also, `find . -name '*.js' | xargs cat | wc -l` would concatenate the files together and give you the
+  //REVIEW: output in a single line (and result.trim() to get rid of the newlines around it)
   const filteredResult = result.split( /\r?\n/ ).filter( string => string );
   const linesOfCode = filteredResult && filteredResult.pop();
   const linesOfCodeFormatted = linesOfCode ? linesOfCode.split( ' ' ).filter( string => string ).shift() : '0';
@@ -54,7 +57,11 @@ const formatCodeCount = result => {
 
 // filter and parse stdout to return word count in each repo
 const formatWordCount = result => {
+  //REVIEW: We usually use Number.parseInt or the parseInt global to cast things to a number (though this might work
+  //REVIEW: better, so please let me know if we should update what we usually do)
   const instances = result.split( /\r?\n/ ).map( string => Number( string.slice( -1 ) ) );
+  //REVIEW: return _.sum( instances ) --- since it seems like instances is an array of JUST numbers? Not sure why
+  //REVIEW: there is the `instance &&` check. If needed, `return _.sum( instances.filter( _.identity ) )`
   let count = 0;
   instances.forEach( instance => {
     instance && ( count += instance );
@@ -63,6 +70,7 @@ const formatWordCount = result => {
 };
 
 const percent = ( numerator, denominator ) => {
+  //REVIEW: For this type of thing, I'd use Math.floor (so that it truly needs to be 100% to be shown as 100%)
   return Math.round( ( numerator / denominator ) * 100 );
 };
 
@@ -70,6 +78,8 @@ const tableData = {};
 
 // iterate through list of common code repos to fill out data
 repos.forEach( repo => {
+  //REVIEW: Recommend potentially async/await style code with loading files in as a string (as long as it doesn't kill
+  //REVIEW: performance. Collaboration on that sounds good!
   const jsResult = child_process.execSync( 'find . -name \'*.js\' | xargs wc -l', { cwd: `${repo}/js` } );
   const tsResult = child_process.execSync( 'find . -name \'*.ts\' | xargs wc -l', { cwd: `${repo}/js` } );
   const tsIgnoreResult = child_process.spawnSync( 'grep -r -c --include="*.ts" -w @ts-ignore', { cwd: `${repo}/js`, shell: true } );
@@ -88,6 +98,14 @@ repos.forEach( repo => {
     [ privateHeader ]: privateCount
   };
 } );
+
+//REVIEW: Consider using lodash instead of the iteration:
+// const _ = require( 'lodash' ); // above
+// const rows = Object.values( tableData );
+// const totalJS = _.sumBy( rows, jsHeader );
+// const totalTS = _.sumBy( rows, tsHeader );
+// const totalTSIgnore = _.sumBy( rows, tsIgnoreHeader );
+// const totalPrivate = _.sumBy( rows, privateHeader );
 
 let totalJS = 0;
 let totalTS = 0;
