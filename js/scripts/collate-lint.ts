c963845d0e0e9cf7320bcbc959a0eba5ddf8ec96
Sam Reid <reids@colorado.edu>
2022-07-11 17:16:21 -0600

    Add notes about disabled typescript rules (that don't use type info), see https://github.com/phetsims/chipper/issues/1277

---
+++ js/scripts/collate-lint.ts
@@ -0,0 +1,36 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * This script uses Deno and is run like so:
+ *
+ * grunt lint-everything > lintreport.txt
+ * deno run --allow-read js/scripts/collate-lint.ts
+ *
+ * It counts failures of each rule
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+const text = Deno.readTextFileSync( './lintreport.txt' );
+
+const lines = text.split( '\n' );
+let keys = [];
+lines.forEach( line => {
+  if ( line.includes( '@typescript-eslint/' ) ) {
+    const key = line.substring( line.indexOf( '@typescript-eslint/' ) );
+    if ( !keys.includes( key ) ) {
+      keys.push( key );
+    }
+  }
+} );
+console.log( keys );
+keys = keys.sort();
+
+keys.forEach( key => {
+  let count = 0;
+  lines.forEach( line => {
+    if ( line.includes( key ) ) {
+      count++;
+    }
+  } );
+  console.log( key, count );
+} );
