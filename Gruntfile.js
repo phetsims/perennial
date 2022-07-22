30df177accbfe45a66c85def0ee838f87e44f138
zepumph <michael.kauzmann@colorado.edu>
2022-07-21 19:36:06 -0600

    use perennial-alias for all linting to preserve caching no matter where we call from, https://github.com/phetsims/chipper/issues/1286

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 // Copyright 2002-2015, University of Colorado Boulder
 
 
-module.exports = require( '../perennial/js/grunt/Gruntfile.js' );
+// Use `./` instead of `../perennial` so that perennial-alias uses its own gruntfile
+module.exports = require( './js/grunt/Gruntfile.js' );
