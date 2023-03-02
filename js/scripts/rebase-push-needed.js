e4f10f4714aa78870c6fdb7943c92325218d8947
Jonathan Olson <jonathan.olson@colorado.edu>
2023-03-01 18:44:29 -0700

    Adding rebase-push-needed, for quick pushes with a rebase guaranteed, only hitting the remote where necessary

---
+++ js/scripts/rebase-push-needed.js
@@ -0,0 +1,68 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Checks status for repos, and prints it out to the console
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+const execute = require( '../common/execute' );
+const getActiveRepos = require( '../common/getActiveRepos' );
+const gitIsClean = require( '../common/gitIsClean' );
+const gitPullRebase = require( '../common/gitPullRebase' );
+const gitPush = require( '../common/gitPush' );
+const winston = require( 'winston' );
+
+winston.default.transports.console.level = 'error';
+
+// ANSI escape sequences to move to the right (in the same line) or to apply or reset colors
+const red = '\u001b[31m';
+const green = '\u001b[32m';
+const reset = '\u001b[0m';
+
+const repos = getActiveRepos();
+const data = {};
+let ok = true;
+
+const rebasePushNeeded = async repo => {
+  data[ repo ] = '';
+
+  try {
+    const symbolicRef = ( await execute( 'git', [ 'symbolic-ref', '-q', 'HEAD' ], `../${repo}` ) ).trim();
+    const branch = symbolicRef.replace( 'refs/heads/', '' );
+    const trackShort = branch ? ( await execute( 'git', [ 'for-each-ref', '--format=%(push:trackshort)', symbolicRef ], `../${repo}` ) ).trim() : '';
+
+    // If it's ahead at all
+    if ( trackShort.includes( '>' ) ) {
+      if ( await gitIsClean( repo ) ) {
+        await gitPullRebase( repo );
+      }
+      else {
+        data[ repo ] += `${red}${repo} not clean, skipping pull${reset}\n`;
+      }
+
+      if ( branch ) {
+        await gitPush( repo, branch );
+        data[ repo ] += `${green}${repo} pushed\n`;
+      }
+      else {
+        data[ repo ] += `${red}${repo} no branch, skipping push${reset}\n`;
+        ok = false;
+      }
+    }
+  }
+  catch( e ) {
+    data[ repo ] += `${repo} ERROR: ${e}\n`;
+    ok = false;
+  }
+};
+
+( async () => {
+  await Promise.all( repos.map( repo => rebasePushNeeded( repo ) ) );
+
+  repos.forEach( repo => {
+    process.stdout.write( data[ repo ] );
+  } );
+
+  console.log( `\n${ok ? green : red}-----=====] finished [=====-----${reset}\n` );
+} )();
