9e4f88dbe7c234016498a7087d95f975d771c003
Sam Reid <reids@colorado.edu>
2023-04-03 13:35:14 -0600

    Change query parameter shuffleListeners => listenerOrder, see https://github.com/phetsims/axon/issues/215

--- js/listContinuousTests.js
+++ js/listContinuousTests.js
@@ -96,10 +96,10 @@ runnableRepos.forEach( repo => {
   } );
 
   tests.push( {
-    test: [ repo, 'fuzz', 'unbuilt', 'shuffleListeners' ],
+    test: [ repo, 'fuzz', 'unbuilt', 'listenerOrderRandom' ],
     type: 'sim-test',
     url: `${repo}/${repo}_en.html`,
-    queryParameters: 'brand=phet&ea&fuzz&shuffleListeners&memoryLimit=1000',
+    queryParameters: 'brand=phet&ea&fuzz&listenerOrder=random&memoryLimit=1000',
     priority: 0.3
   } );
 
