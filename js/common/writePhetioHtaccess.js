31796a60e6b67dbe6c98357989d6ae1600c8891a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-13 12:04:57 -0600

    swap allowPublicAccess from graphing-quadratics to density, https://github.com/phetsims/phet-io-website/issues/117
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/common/writePhetioHtaccess.js
+++ js/common/writePhetioHtaccess.js
@@ -61,31 +61,15 @@ module.exports = async function writePhetioHtaccess( passwordProtectPath, latest
   const getSubdirHtaccessFullPath = subdir => `${passwordProtectPath}/${getSubdirHtaccessPath( subdir )}`;
   const rootHtaccessFullPath = `${passwordProtectPath}/${htaccessFilename}`;
 
-  // Only skip htaccess creation if in production deploy when the "allowPublicAccess" flag is present
-  // If we are allowing public access, make sure that the htaccess files don't already exist locally already. This can
-  // occur when an rc is published (first) during a production deploy.
+  // Only allow public accessibility with htaccess mutation if in production deploy when the "allowPublicAccess" flag
+  // is present. Commented out lines keep password protection, but comment them in with `allowPublicAccess`.
+  let commentSymbol = '#';
+
   if ( simPackage && simPackage.phet && simPackage.phet[ 'phet-io' ] && simPackage.phet[ 'phet-io' ].allowPublicAccess ) {
-
-    for ( const subdir of PASSWORD_PROTECTED_SUB_DIRS ) {
-
-      // Use individual try/catch blocks to ensure atomic operations.
-      try {
-        await fs.unlinkSync( getSubdirHtaccessFullPath( subdir ) );
-      }
-      catch( e ) {
-        winston.debug( `did not remove ${subdir} htaccess ${e}` );
-      }
-    }
-    try {
-      await fs.unlinkSync( rootHtaccessFullPath );
-    }
-    catch( e ) {
-      winston.debug( `did not remove root htaccess ${e}` );
-    }
+    commentSymbol = '';
   }
-  else {
-    try {
-      const basePasswordProtectContents = `
+  try {
+    const basePasswordProtectContents = `
 AuthType Basic
 AuthName "PhET-iO Password Protected Area"
 AuthUserFile ${authFilepath}
@@ -94,49 +78,48 @@ AuthUserFile ${authFilepath}
 </LimitExcept>
 `;
 
-      const passwordProtectWrapperContents = `${basePasswordProtectContents}\n
+    const passwordProtectWrapperContents = `${basePasswordProtectContents}\n
           
 # Uncomment these lines to support public access to this "wrappers/" directory
-# Satisfy Any
-# Allow from all
+${commentSymbol} Satisfy Any
+${commentSymbol} Allow from all
 `;
 
-      // Write a file to add authentication to subdirectories like wrappers/ or doc/
-      for ( const subdir of PASSWORD_PROTECTED_SUB_DIRS ) {
-        const htaccessPathToDir = getSubdirHtaccessFullPath( subdir );
+    // Write a file to add authentication to subdirectories like wrappers/ or doc/
+    for ( const subdir of PASSWORD_PROTECTED_SUB_DIRS ) {
+      const htaccessPathToDir = getSubdirHtaccessFullPath( subdir );
 
-        // if the directory exists
-        if ( fs.existsSync( htaccessPathToDir.replace( htaccessFilename, '' ) ) ) {
+      // if the directory exists
+      if ( fs.existsSync( htaccessPathToDir.replace( htaccessFilename, '' ) ) ) {
 
-          await writeFile( htaccessPathToDir, passwordProtectWrapperContents );
-          if ( devVersionPath ) {
-            await devScp( htaccessPathToDir, `${devVersionPath}/phet-io/${getSubdirHtaccessPath( subdir )}` );
-          }
+        await writeFile( htaccessPathToDir, passwordProtectWrapperContents );
+        if ( devVersionPath ) {
+          await devScp( htaccessPathToDir, `${devVersionPath}/phet-io/${getSubdirHtaccessPath( subdir )}` );
         }
       }
+    }
 
-      const phetioPackage = JSON.parse( fs.readFileSync( '../phet-io/package.json' ) );
+    const phetioPackage = JSON.parse( fs.readFileSync( '../phet-io/package.json' ) );
 
-      // Write a file to add authentication to the top level index pages
-      if ( phetioPackage.phet && phetioPackage.phet.addRootHTAccessFile ) {
-        const passwordProtectIndexContents = `<FilesMatch "(index\\.\\w+|api\\.json)$">\n${
-          basePasswordProtectContents
-        }</FilesMatch>
+    // Write a file to add authentication to the top level index pages
+    if ( phetioPackage.phet && phetioPackage.phet.addRootHTAccessFile ) {
+      const passwordProtectIndexContents = `<FilesMatch "(index\\.\\w+|api\\.json)$">\n${
+        basePasswordProtectContents
+      }</FilesMatch>
                         
 # Uncomment these lines to support public access to all resources in this version (including wrappers)
-# Satisfy Any
-# Allow from all
+${commentSymbol} Satisfy Any
+${commentSymbol} Allow from all
 `;
-        await writeFile( rootHtaccessFullPath, passwordProtectIndexContents );
-        if ( devVersionPath ) {
-          await devScp( rootHtaccessFullPath, `${devVersionPath}/phet-io/${htaccessFilename}` );
-        }
+      await writeFile( rootHtaccessFullPath, passwordProtectIndexContents );
+      if ( devVersionPath ) {
+        await devScp( rootHtaccessFullPath, `${devVersionPath}/phet-io/${htaccessFilename}` );
       }
-      winston.debug( 'phetio authentication htaccess written' );
     }
-    catch( err ) {
-      winston.debug( 'phetio authentication htaccess not written' );
-      throw err;
-    }
+    winston.debug( 'phetio authentication htaccess written' );
+  }
+  catch( err ) {
+    winston.debug( 'phetio authentication htaccess not written' );
+    throw err;
   }
 };
