d9d78b205f81d2398ec6d67089a62f708e03e010
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-22 12:25:44 -0600

    update test (failIfNoLoad for now), https://github.com/phetsims/phet-io/issues/1944

--- js/listContinuousTests.js
+++ js/listContinuousTests.js
@@ -513,7 +513,8 @@ tests.push( {
   test: [ 'molecule-polarity', 'migration', '1.2->master' ],
   type: 'wrapper-test',
   url: 'phet-io-wrappers/migration/?sim=molecule-polarity&locales=*&phetioDebug=true&phetioWrapperDebug=true&fuzz&oldVersion=1.2&migrationRate=1000&silent',
-  priority: 100  // Just while we are initially testing in, https://github.com/phetsims/phet-io/issues/1944
+  priority: 100,  // Just while we are initially testing in, https://github.com/phetsims/phet-io/issues/1944
+  testQueryParameters: 'failIfNoLoad' // Just while we are initially testing in, https://github.com/phetsims/phet-io/issues/1944
 } );
 
 console.log( JSON.stringify( tests, null, 2 ) );
