759058b4320cf97ef379db159366cfd20c8261e8
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-29 11:11:11 -0600

    remove temp code for testing, https://github.com/phetsims/phet-io/issues/1944

--- js/listContinuousTests.js
+++ js/listContinuousTests.js
@@ -427,8 +427,7 @@ Object.keys( commonQueryParameters ).forEach( name => {
   tests.push( {
     test: [ simName, 'migration', `${oldVersion}->master` ],
     type: 'wrapper-test',
-    url: `phet-io-wrappers/migration/?sim=${simName}&locales=*&phetioDebug=true&phetioWrapperDebug=true&fuzz&oldVersion=${oldVersion}&migrationRate=1000&silent`,
-    priority: 101  // Just while we are initially testing, TODO: remove in https://github.com/phetsims/phet-io/issues/1944
+    url: `phet-io-wrappers/migration/?sim=${simName}&locales=*&phetioDebug=true&phetioWrapperDebug=true&fuzz&oldVersion=${oldVersion}&migrationRate=1000&silent`
   } );
 } );
 ////////////////////////////////////////////
