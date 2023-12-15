9b0155cceb0b5c6cf76573a6df14978022791a1a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-12-15 12:19:12 -0700

    add --branch as an alias to checkout-target, https://github.com/phetsims/perennial/issues/342

--- js/grunt/Gruntfile.js
+++ js/grunt/Gruntfile.js
@@ -96,18 +96,21 @@ module.exports = function( grunt ) {
     'Check out a specific branch/SHA for a simulation and all of its declared dependencies\n' +
     '--repo : repository name where package.json should be read from\n' +
     '--target : the branch/SHA to check out\n' +
+    '--branch : alias for --target\n' +
     '--skipNpmUpdate : If provided, will prevent the usual npm update',
     wrapTask( async () => {
       const repo = grunt.option( 'repo' );
 
       assert( repo, 'Requires specifying a repository with --repo={{REPOSITORY}}' );
-      assert( grunt.option( 'target' ), 'Requires specifying a branch/SHA with --target={{BRANCH}}' );
+      assert( !( grunt.option( 'target' ) && grunt.option( 'branch' ) ), '--target and --branch are the same option, only use one.' );
+      const target = grunt.option( 'target' ) || grunt.option( 'branch' );
+      assert( target, 'Requires specifying a branch/SHA with --target={{BRANCH}}' );
 
       assertIsValidRepoName( repo );
 
       const checkoutTarget = require( '../common/checkoutTarget' );
 
-      await checkoutTarget( repo, grunt.option( 'target' ), !grunt.option( 'skipNpmUpdate' ) );
+      await checkoutTarget( repo, target, !grunt.option( 'skipNpmUpdate' ) );
     } ) );
 
   grunt.registerTask( 'checkout-release',
