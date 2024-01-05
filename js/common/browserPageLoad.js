861184d1bd0f1f116bf1d53606334e227b616607
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-01-05 15:56:19 -0700

    runTest should use browserPageLoad, https://github.com/phetsims/aqua/issues/198

--- js/common/browserPageLoad.js
+++ js/common/browserPageLoad.js
@@ -1,14 +1,24 @@
 // Copyright 2017-2022, University of Colorado Boulder
 
 /**
- * Uses puppeteer to see whether a page loads without an error. Throws errors it receives
+ * Uses a browser to see whether a page loads without an error. Throws errors it receives.
+ *
+ * Supports multiple supported browsers from puppeteer and playwright. Must provide a browserCreator from either with a
+ * `launch()` interface.
+ * There are now many more features of this class. It is best to see its functionality by looking at options.
+ *
+ * To support authentication, we use process.env.BASIC_PASSWORD and process.env.BASIC_USERNAME, set those before calling
+ * this function.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
 const sleep = require( './sleep' );
 const _ = require( 'lodash' );
 const winston = require( 'winston' );
+const puppeteer = require( 'puppeteer' );
+const assert = require( 'assert' );
 
 /**
  * Uses puppeteer to see whether a page loads without an error
@@ -22,7 +32,7 @@ const winston = require( 'winston' );
  * @returns {Promise.<null|*>} - The eval result/null
  */
 module.exports = async function( browserCreator, url, options ) {
-
+  let pageLoaded = false;
   options = _.merge( {
 
     // See https://github.com/puppeteer/puppeteer/blob/v14.1.1/docs/api.md#puppeteerlaunchoptions
@@ -33,20 +43,37 @@ module.exports = async function( browserCreator, url, options ) {
       ]
     },
 
-    browser: null, // {puppeteer.Browser|null} - If provided, we'll use a persistent browser
+    browser: null, // If provided, browserCreator is not used to create a browser, and this browser is not closed.
 
     evaluate: null, // {function|null}
     waitForFunction: null, // {string|null}
 
     rejectPageErrors: true, // reject when the page errors
+    rejectErrors: true, // reject when there is an error with the browser
+
+    // By default, once loaded we resolve, but opt out of this here. If you set to false, you must resolve in custom logic in onPageCreation
+    resolveFromLoad: true,
     waitAfterLoad: 5000, // milliseconds
     allowedTimeToLoad: 40000, // milliseconds
     gotoTimeout: 30000, // milliseconds
 
+    // Callback when logic is not complete after timeout of length: allowedTimeToLoad.
+    onLoadTimeout: ( resolve, reject ) => {
+      if ( !pageLoaded ) {
+        options.logger( 'puppeteer page not loaded' );
+        reject( new Error( `Did not load in ${options.allowedTimeToLoad}` ) );
+      }
+    },
+    onPageCreation: null, // {function(page, resolve,reject):Promise<void>|null} - any extra items you want to do with the page before goto is called
+    evaluateOnNewDocument: null, // {function|null} page.evaluateOnNewDocument for puppeteer, and addInitScript for playwrite
+
+    cachePages: true,
     logConsoleOutput: false, // if true, this process will log all messages that come from page.on( 'console' )
     logNavigation: false, // if true, this process will log all messages that come from page.on( 'frame*' )
-    logger: winston.info // pass in `console.log` if you are running in a context that doesn't use winston
+    logger: winston.info // {function(message)} pass in `console.log` if you are running in a context that doesn't use winston
   }, options );
+
+  !options.resolveFromLoad && assert( options.onPageCreation, 'must resolve from onPageCreation' );
 
   const ownsBrowser = !options.browser;
 
@@ -64,7 +91,16 @@ module.exports = async function( browserCreator, url, options ) {
     browser = options.browser || await browserCreator.launch( options.launchOptions );
 
     page = await browser.newPage();
+
+    page.setCacheEnabled && page.setCacheEnabled( options.cachePages );
+
     await page.setDefaultNavigationTimeout( options.gotoTimeout );
+
+    // The API for playwright was much more complicated, so just support puppeteer
+    if ( !options.browser && browserCreator === puppeteer &&
+         process.env.BASIC_PASSWORD && process.env.BASIC_USERNAME ) {
+      await page.authenticate( { username: process.env.BASIC_USERNAME, password: process.env.BASIC_PASSWORD } );
+    }
 
     // promote for use outside the closure
     let resolve;
@@ -74,18 +110,24 @@ module.exports = async function( browserCreator, url, options ) {
       reject = rej;
     } );
 
-    let loaded = false;
     page.on( 'response', async response => {
-
       // 200 and 300 class status are most likely fine here
       if ( response.url() === url && response.status() >= 400 ) {
-        options.logger( `Could not load from status: ${response.status()}` );
+        options.logger( `[ERROR] Could not load from status: ${response.status()}` );
       }
     } );
-    options.logConsoleOutput && page.on( 'console', msg => console.log( msg.text() ) );
+    options.logConsoleOutput && page.on( 'console', msg => {
+      let messageTxt = msg.text();
+
+      // Append the location to messages that would benefit from it.
+      if ( messageTxt.includes( 'net:' ) || messageTxt.includes( 'Failed to load resource' ) ) {
+        messageTxt += `: \t ${msg.location().url}`;
+      }
+      options.logger( `[CONSOLE] ${messageTxt}` );
+    } );
 
     page.on( 'load', async () => {
-      loaded = true;
+      pageLoaded = true;
       await sleep( options.waitAfterLoad );
       if ( options.waitForFunction ) {
         await page.waitForFunction( options.waitForFunction, {
@@ -93,36 +135,46 @@ module.exports = async function( browserCreator, url, options ) {
           timeout: options.gotoTimeout
         } );
       }
-      resolve( options.evaluate && !page.isClosed() ? await page.evaluate( options.evaluate ) : null );
+      options.resolveFromLoad && resolve( options.evaluate && !page.isClosed() ? await page.evaluate( options.evaluate ) : null );
     } );
+
+    options.onPageCreation && await options.onPageCreation( page, resolve, reject );
+
+    // Support puppeteer (evaluateOnNewDocument) or playwright (addInitScript)
+    options.evaluateOnNewDocument && await ( ( page.evaluateOnNewDocument || page.addInitScript ).call( page, options.evaluateOnNewDocument ) );
+
+
     page.on( 'error', message => {
-      options.logger( `puppeteer error: ${message}` );
-      reject( new Error( message ) );
+      options.logger( `[ERROR] ${message}` );
+      if ( options.rejectErrors ) {
+        reject( new Error( message ) );
+      }
     } );
     page.on( 'pageerror', message => {
+      options.logger( `[PAGE ERROR] ${message}` );
       if ( options.rejectPageErrors ) {
-        options.logger( `puppeteer pageerror: ${message}` );
         reject( new Error( message ) );
       }
     } );
     if ( options.logNavigation ) {
       page.on( 'frameattached', async frame => {
-        options.logger( 'attached', frame.url() );
+        options.logger( `[ATTACHED] ${frame.url()}` );
       } );
       page.on( 'framedetached', async frame => {
-        options.logger( 'detached', frame.url() );
+        options.logger( `[DETACHED] ${frame.url()}` );
       } );
       page.on( 'framenavigated', async frame => {
-        options.logger( 'navigated', frame.url() );
+        options.logger( `[NAVIGATED] ${frame.url()}` );
       } );
     }
-    ( async () => {
-      await sleep( options.allowedTimeToLoad );
-      if ( !loaded ) {
-        options.logger( 'puppeteer not loaded' );
-        reject( new Error( `Did not load in ${options.allowedTimeToLoad}` ) );
-      }
-    } )();
+
+    // Use timeout so that you can cancel it once we have a result. Node will wait for this if it is a orphaned Promise.
+    const timeoutID = setTimeout( () => {
+      options.onLoadTimeout( resolve, reject );
+    }, options.allowedTimeToLoad );
+
+    options.logger( `[URL] ${url}` );
+
     let result = null;
 
     // Await both at the same time, because all rejection is hooked up to the `promise`, but that could cause an error
@@ -135,6 +187,7 @@ module.exports = async function( browserCreator, url, options ) {
     ] );
 
     await cleanup();
+    clearTimeout( timeoutID );
     return result;
   }
 
