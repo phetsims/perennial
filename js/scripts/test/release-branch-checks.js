251746f9f078339b5c585095b3ff319ae775c3f5
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-06-28 12:43:37 -0600

    remove hydrogen check, oops, https://github.com/phetsims/joist/issues/970
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/scripts/test/release-branch-checks.js
+++ js/scripts/test/release-branch-checks.js
@@ -193,10 +193,6 @@ const logResult = ( success, message, url ) => {
 
       console.log( '-', releaseBranch ? releaseBranch.toString() : repo );
 
-      if ( !( await releaseBranch.isPhetioHydrogen() ) ) {
-        continue;
-      }
-
       for ( const url of urls ) {
 
         // Because `planet` and `planet.controls.title` keys present in translations OVERLAP in the string object created by
