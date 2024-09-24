fa490311c883b8da414d08afb0340c765d73a14a
Sam Reid <reids@colorado.edu>
2024-09-24 10:42:12 -0600

    Switch to projectService: true, see https://github.com/phetsims/chipper/issues/1474

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,5 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 module.exports = {
-  extends: '../chipper/eslint/node_eslintrc.js',
-  overrides: [
-    {
-      files: [
-        '**/*.ts'
-      ],
-      parserOptions: {
-        project: [
-
-          // Support both perennial and perennial-alias
-          // TODO: This was likely missed when pointing everything to chipper/tsconfig/eslint-all/tsconfig.json, see https://github.com/phetsims/chipper/issues/1468
-          __dirname + '/tsconfig.json'
-        ]
-      }
-    }
-  ]
+  extends: '../chipper/eslint/node_eslintrc.js'
 };
