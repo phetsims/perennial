5d31cc7c4e3bcf608459915e16f3968ccd3d7d72
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-27 09:51:23 -0600

    move grunt/tasks/util functions for perennial, https://github.com/phetsims/chipper/issues/1464

---
+++ js/grunt/tasks/util/buildLocal.ts
@@ -0,0 +1,18 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import * as grunt from 'grunt';
+
+let buildLocal: any;
+try {
+  buildLocal = grunt.file.readJSON( `${process.env.HOME}/.phet/build-local.json` );
+}
+catch( e ) {
+  // Handle the lack of build.json
+  buildLocal = {};
+}
+
+export default buildLocal;
