fd6a7c6f60508064bbd32d31ad27c3373b82fb79
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-27 11:50:41 -0600

    create grunt/tasks files in aqua, https://github.com/phetsims/chipper/issues/1464

---
+++ js/import-shared/grunt.js
@@ -0,0 +1,10 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * To be used when trying to import grunt from other repos. This way we don't propagate the devDependency: "@types/grunt"
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import grunt from 'grunt';
+
+export default grunt;
---
+++ js/import-shared/lodash.js
@@ -0,0 +1,10 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * To be used when trying to import lodash from other repos. This way we don't propagate the devDependency: "@types/lodash"
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import lodash from 'lodash';
+
+export default lodash;
