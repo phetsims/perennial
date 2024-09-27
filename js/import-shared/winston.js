8294f5aa42b808924fd92d6b5de6a5976f902bc5
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-27 11:50:41 -0600

    create grunt/tasks files in aqua, https://github.com/phetsims/chipper/issues/1464

---
+++ js/import-shared/winston.js
@@ -0,0 +1,10 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * To be used when trying to import winston from other repos.
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import winston from 'winston';
+
+export default winston;
