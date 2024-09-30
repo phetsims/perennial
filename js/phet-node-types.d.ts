a9780257f79d7470b0a2be0b3ccd99ae6465c32a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-30 16:59:50 -0600

    Deno is a node global, https://github.com/phetsims/chipper/issues/1463

---
+++ js/phet-node-types.d.ts
@@ -0,0 +1,11 @@
+// Copyright 2024, University of Colorado Boulder
+
+/* eslint-disable no-var */
+
+/**
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+declare var Deno: {
+  readTextFileSync: ( file: string ) => string;
+};
