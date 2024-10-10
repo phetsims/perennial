e8e000376526f2d95214db9e4c3f2fb249c52483
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-10 13:18:12 -0600

    fix up maintenance-create-patch.ts, https://github.com/phetsims/chipper/issues/1461

--- js/grunt/tasks/maintenance-create-patch.ts
+++ js/grunt/tasks/maintenance-create-patch.ts
@@ -1,14 +1,29 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * Reports out on release branch statuses
+ * Adds a patch to the maintenance process
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
+import assert from 'assert';
 import winston from 'winston';
-import Maintenance from '../../common/Maintenance';
+import assertIsValidRepoName from '../../common/assertIsValidRepoName.js';
+import Maintenance from '../../common/Maintenance.js';
+import getOption from './util/getOption';
 
 winston.default.transports.console.level = 'error';
 
 // TODO: Does this belong in grunt? See https://github.com/phetsims/chipper/issues/1461
-( async () => Maintenance.checkBranchStatus() )();
+( async () => {
+
+  const repo = getOption( 'repo' );
+  assertIsValidRepoName( repo );
+
+  const message = getOption( 'message' );
+
+  assert( repo, 'Requires specifying a repo that will need to be patched with --repo={{REPO}}' );
+  assert( message, 'Requires specifying a message (included with commits) with --message={{MESSAGE}}' );
+
+  await Maintenance.createPatch( repo, message );
+} )();
