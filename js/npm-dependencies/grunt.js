0f310fc47ca458be14947fcef570c1a256856cf4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-15 10:10:43 -0600

    doc https://github.com/phetsims/chipper/issues/1464

--- js/npm-dependencies/grunt.js
+++ js/npm-dependencies/grunt.js
@@ -1,7 +1,7 @@
 // Copyright 2019, University of Colorado Boulder
 
 /**
- * To be used when trying to import grunt from other repos. This way we don't propagate the devDependency: "@types/grunt"
+ * To be used when trying to import grunt from other repos. This way we also don't propagate the devDependency: "@types/grunt"
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
