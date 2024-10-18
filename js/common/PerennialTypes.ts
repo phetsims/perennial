6262ace6b55142527e28685a2d858ac7fac04a65
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-18 14:13:48 -0600

    move check() to a module, https://github.com/phetsims/chipper/issues/1487
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

---
+++ js/common/PerennialTypes.ts
@@ -0,0 +1,7 @@
+// Copyright 2024, University of Colorado Boulder
+/**
+ * Collection of usable types for build tools
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+export type Repo = string;
