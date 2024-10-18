89a9fa0e7c9431cbb9d464b3f2f88d48f0aa4262
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-18 13:43:43 -0600

    move grunt check to perennial, https://github.com/phetsims/chipper/issues/1487
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

---
+++ js/common/fixEOL.ts
@@ -0,0 +1,13 @@
+// Copyright 2020-2024, University of Colorado Boulder
+
+/**
+ * Fix end of lines for a string based on the operating system this code is being run on.
+ * See https://github.com/phetsims/chipper/issues/933
+ *
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+// modules
+const os = require( 'os' );
+
+export default ( string: string ): string => string.split( '\r' ).join( '' ).split( '\n' ).join( os.EOL );
