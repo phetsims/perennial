b126251c7c6506092ea36b1b685c6893eae3bca9
Sam Reid <reids@colorado.edu>
2024-10-28 07:57:02 -0600

    Require *.js suffix and allow camel casing name matching, see https://github.com/phetsims/chipper/issues/1498

--- js/eslint/rules/require-statement-match.js
+++ js/eslint/rules/require-statement-match.js
@@ -5,17 +5,66 @@
  * @copyright 2015 University of Colorado Boulder
  */
 
-
-//------------------------------------------------------------------------------
-// Rule Definition
-//------------------------------------------------------------------------------
+const path = require( 'path' );
 
 module.exports = {
+  meta: {
+    type: 'suggestion', // 'problem', 'suggestion', or 'layout'
+    docs: {
+      description: 'Ensure require statement variable names match the module names',
+      category: 'Best Practices',
+      recommended: false
+    },
+    fixable: 'code', // 'code' or 'whitespace'
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          exceptions: {
+            type: 'object',
+            additionalProperties: {
+              type: 'string'
+            }
+          }
+        },
+        additionalProperties: false
+      }
+    ]
+  },
+
   create: function( context ) {
 
-    return {
 
-      VariableDeclaration: function( node ) {
+    // Get a list of built-in Node.js modules
+    const builtInModules = new Set( module.builtinModules );
+
+    // Retrieve custom exceptions from rule options
+    const options = context.options[ 0 ] || {};
+    const exceptionModules = options.exceptions || {
+      lodash: '_',
+      underscore: '_',
+      jquery: '$',
+      'lodash-4.17': '_' // Note it is imported as lodash-4.17.4, but it mistakes .4 as the extension
+    };
+
+    function toCamelCase( str ) {
+      return str.replace( /[-_](.)/g, ( _, group1 ) => group1.toUpperCase() );
+    }
+
+    function getModuleName( requirePath ) {
+      // Handle scoped packages like '@namespace/module-name'
+      if ( requirePath.startsWith( '@' ) ) {
+        const parts = requirePath.split( '/' );
+        return parts.length > 1 ? parts[ 1 ] : parts[ 0 ];
+      }
+      else {
+        const parts = requirePath.split( '/' );
+        return parts[ parts.length - 1 ];
+      }
+    }
+
+    return {
+      VariableDeclaration( node ) {
 
         // Here is the AST of a typical require statement node, for reference
         //var exemplar = {
@@ -45,43 +94,79 @@ module.exports = {
         //  ],
         //  'kind': 'var'
         //};
+        node.declarations.forEach( declaration => {
+          const init = declaration.init;
+          if (
+            init &&
+            init.type === 'CallExpression' &&
+            init.callee.name === 'require' &&
+            init.arguments.length === 1 &&
+            init.arguments[ 0 ].type === 'Literal' &&
+            typeof init.arguments[ 0 ].value === 'string' &&
+            declaration.id.type === 'Identifier' // Skip destructuring assignments
+          ) {
+            const lhs = declaration.id.name;
+            const rhs = init.arguments[ 0 ].value;
 
-        if ( node.declarations &&
-             node.declarations.length > 0 &&
-             node.declarations[ 0 ].init &&
-             node.declarations[ 0 ].init.arguments &&
-             node.declarations[ 0 ].init.arguments.length > 0 ) {
-          if ( node.declarations[ 0 ].init &&
-               node.declarations[ 0 ].init.callee.name === 'require' &&
-               node.declarations[ 0 ].id.type !== 'ObjectPattern' ) { // don't error out for const { hi }
-            const lhs = node.declarations[ 0 ].id.name;
-            const rhs = node.declarations[ 0 ].init.arguments[ 0 ].value;
+            // Determine if the module is built-in
+            const isBuiltIn = builtInModules.has( rhs );
 
-            if ( rhs && rhs.indexOf( '!' ) < 0 ) {
-              const lastSlash = rhs.lastIndexOf( '/' );
-              let tail = rhs.substring( lastSlash + 1 );
+            // Extract the module name from the require path
+            let moduleName;
 
-              // Support keeping a js extension in require statements
-              if ( tail.includes( '.' ) ) {
-                tail = tail.substring( 0, tail.lastIndexOf( '.' ) );
+            if ( rhs.startsWith( '.' ) ) {
+              // Relative path
+              moduleName = path.basename( rhs );
+
+              // Check for and remove file extension
+              const extName = path.extname( moduleName );
+              const hasExtension = extName.length > 0;
+              if ( hasExtension ) {
+                moduleName = moduleName.slice( 0, -extName.length );
               }
 
-              const isLodash = lhs === '_';
-              if ( tail !== lhs && !isLodash ) {
+              // Optionally enforce .js suffix for relative paths
+              const enforceJsSuffix = true; // Set to false if not enforcing
+
+              if ( enforceJsSuffix && !hasExtension ) {
                 context.report( {
-                  node: node,
-                  loc: node.loc.start,
-                  message: `Mismatched require statement values, ${lhs} !== ${tail}`
+                  node: init.arguments[ 0 ],
+                  message: `Require path '${rhs}' should include '.js' extension.`,
+                  fix: function( fixer ) {
+                    return fixer.replaceText(
+                      init.arguments[ 0 ],
+                      `'${rhs}.js'`
+                    );
+                  }
                 } );
               }
             }
+            else if ( !isBuiltIn ) {
+              // External module from node_modules or scoped package
+              moduleName = getModuleName( rhs );
+            }
+            else {
+              // Built-in module, skip variable name check
+              return;
+            }
+
+            // Convert moduleName to camelCase for comparison
+            const camelCaseModuleName = toCamelCase( moduleName );
+
+            // Determine the expected variable name
+            const expectedVariableName = exceptionModules[ moduleName ] || camelCaseModuleName;
+
+            // Compare the LHS variable name with the expected variable name
+            if ( lhs !== expectedVariableName && lhs !== moduleName ) {
+              context.report( {
+                node: declaration.id,
+                message: `Variable name '${lhs}' does not match module name '${moduleName}'. Expected '${expectedVariableName} or ${moduleName}'.`
+                // No autofix for variable names to avoid unintended side effects
+              } );
+            }
           }
-        }
+        } );
       }
     };
   }
 };
-
-module.exports.schema = [
-  // JSON Schema for rule options goes here
-];
