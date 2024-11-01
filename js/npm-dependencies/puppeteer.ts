aaa3d80cf26192a847c94eb8ee9dacd39d3088a6
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-01 13:08:06 -0600

    quickserver should use puppeteer from perennial, https://github.com/phetsims/aqua/issues/219

---
+++ js/npm-dependencies/puppeteer.ts
@@ -0,0 +1,10 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * To be used when trying to import puppeteer from other repos.
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import puppeteer from 'puppeteer';
+
+export default puppeteer;
