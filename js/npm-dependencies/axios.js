d02f4e95a59fa44b705530f27c97d98324737ab5
Sam Reid <reids@colorado.edu>
2024-11-06 16:52:06 -0700

    Use perennial-alias axios, see https://github.com/phetsims/perennial/issues/372

---
+++ js/npm-dependencies/axios.js
@@ -0,0 +1,11 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * To be used when trying to import axios from other repos.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+const axios = require( 'axios' );
+
+module.exports = axios;
