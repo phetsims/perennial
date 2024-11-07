307dc05c7f4df65cbaa3e8b63d8fdcc9f9f05fe8
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-07 16:24:13 -0700

    inline module and convert to typescript, https://github.com/phetsims/perennial/issues/370

--- js/grunt/tasks/checkout-common-timestamp.ts
+++ js/grunt/tasks/checkout-common-timestamp.ts
@@ -5,15 +5,61 @@
  * --timestamp : the timestamp to check things out for, e.g. --timestamp="Jan 08 2018"
  * --skipNpmUpdate : If provided, will prevent the usual npm update
  *
- * TODO: Move to perennial/js/scripts and inline module, https://github.com/phetsims/perennial/issues/370
- *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
+
 import assert from 'assert';
-import checkoutCommonTimestamp from '../../common/checkoutCommonTimestamp';
+import _ from 'lodash';
+import winston from 'winston';
+import getRepoList from '../../common/getRepoList.js';
+import gitFetchCheckout from '../../common/gitFetchCheckout.js';
+import gitFromTimestamp from '../../common/gitFromTimestamp.js';
+import npmUpdate from '../../common/npmUpdate.js';
 import getOption from './util/getOption';
 
 ( async () => {
-  assert( getOption( 'timestamp' ), 'Requires specifying a timestamp with --timestamp={{BRANCH}}' );
-  await checkoutCommonTimestamp( getOption( 'timestamp' ), !getOption( 'skipNpmUpdate' ) );
+  const timestamp = getOption( 'timestamp' );
+  assert( timestamp, 'Requires specifying a timestamp with --timestamp={{BRANCH}}' );
+  const includeNpmUpdate = !getOption( 'skipNpmUpdate' );
+
+  const repos = _.uniq( [
+    ...getRepoList( 'active-common-sim-repos' ),
+    'assert',
+    'brand',
+    'joist',
+    'query-string-machine',
+    'sherpa',
+    'utterance-queue',
+    'phet-core',
+    'tandem',
+    'axon',
+    'dot',
+    'kite',
+    'scenery',
+    'scenery-phet',
+    'sun',
+    'twixt',
+    'phetcommon',
+    'phet-lib',
+    'chipper',
+    'perennial-alias',
+    'phetmarks'
+  ] ).sort();
+
+  for ( const repo of repos ) {
+    winston.info( repo );
+
+    try {
+      const sha = await gitFromTimestamp( repo, 'main', timestamp );
+      await gitFetchCheckout( repo, sha );
+    }
+    catch( e ) {
+      winston.error( `skipping ${repo}: ${e}` );
+    }
+  }
+
+  if ( includeNpmUpdate ) {
+    await npmUpdate( 'chipper' );
+    await npmUpdate( 'perennial-alias' );
+  }
 } )();
