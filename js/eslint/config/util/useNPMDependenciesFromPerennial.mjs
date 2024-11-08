5972b10d4e38f46cceb33d323d02cd8fd15ac8bd
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-08 08:43:50 -0700

    lint error when importing duplicated installed modules, https://github.com/phetsims/perennial/issues/372

---
+++ js/eslint/config/util/useNPMDependenciesFromPerennial.mjs
@@ -0,0 +1,20 @@
+// Copyright 2024, University of Colorado Boulder
+
+
+/**
+ * Rule that prevents importing from installed node_modules instead of reusing the installation from perennial
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+export default {
+  rules: {
+    'no-restricted-imports': [
+      'error', ...[ 'puppeteer', 'winston', 'axios' ].map( name => {
+        return {
+          name: name,
+          message: 'Prefer importing from perennial/js/npm-dependencies instead of installing a separate copy'
+        };
+      } )
+    ]
+  }
+};
