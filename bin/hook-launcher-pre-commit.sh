ebad58705cbf3f825157737fc9f4c7b9094838b0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-18 13:06:32 -0700

    move pre-commit logic to separate bash file, https://github.com/phetsims/perennial/issues/404

---
+++ bin/hook-launcher-pre-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#-----------------------------------------------------------------------------------------------------------------------
+# Launches the git hooks for pre-commit. This file exists so that its logic can be changed without the need
+# to reinstall the hooks to every repo.
+#
+# Please see https://github.com/phetsims/phet-info/blob/main/doc/phet-development-overview.md#utilities-and-instrumentation-for-development-and-testing
+# for installation instructions.
+#
+# @author Michael Kauzmann (PhET Interactive Simulations)
+# @author Sam Reid (PhET Interactive Simulations)
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Detect the current branch. Requires git 2.22.0 or higher
+current_branch=$(git branch --show-current)
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Only run pre-commit hooks in main, see https://github.com/phetsims/perennial/issues/276
+if [[ "$current_branch" == "main" ]]; then
+ "$SCRIPT_DIR/sage" run "$SCRIPT_DIR/../../chipper/js/scripts/hook-pre-commit.ts"
+fi
