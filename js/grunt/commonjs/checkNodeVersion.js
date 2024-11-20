f35ea3f61af02868d1757328c863e7e9d612191a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-20 16:08:27 -0700

    update node version for the project, https://github.com/phetsims/chipper/issues/1458

--- js/grunt/commonjs/checkNodeVersion.js
+++ js/grunt/commonjs/checkNodeVersion.js
@@ -10,6 +10,8 @@
 const NODE_VERSION_STRING_PARTS = process.version.replace( 'v', '' ).split( '.' );
 const NODE_MAJOR_VERSION = Number( NODE_VERSION_STRING_PARTS[ 0 ] );
 const NODE_MINOR_VERSION = Number( NODE_VERSION_STRING_PARTS[ 1 ] );
-if ( NODE_MAJOR_VERSION < 8 || ( NODE_MAJOR_VERSION === 8 && NODE_MINOR_VERSION < 10 ) ) {
-  throw new Error( 'Node 8.10 or greater is needed to run PhET build tools' );
+if ( NODE_MAJOR_VERSION < 18 ||
+     ( NODE_MAJOR_VERSION === 18 && NODE_MINOR_VERSION < 18 ) ||
+     ( NODE_MAJOR_VERSION === 20 && NODE_MINOR_VERSION < 9 ) ) {
+  throw new Error( 'Node 18.18 or 20.9 or greater is needed to run PhET build tools' );
 }
