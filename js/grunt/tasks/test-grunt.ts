d7b9c68125fa26ec77009e6549a2a324036a552b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-26 14:58:26 -0700

    use --tsc=false, https://github.com/phetsims/chipper/issues/1533

--- js/grunt/tasks/test-grunt.ts
+++ js/grunt/tasks/test-grunt.ts
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 /**
  * Run internal tests for the Gruntfile. Note the output is reported over console.log, so be careful what you output.
- * The command invoked is something like this: execSync( `${gruntCommand} test-grunt --brands=a,b,c --lint=false --noTSC` )
+ * The command invoked is something like this: execSync( `${gruntCommand} test-grunt --brands=a,b,c --lint=false --tsc=false` )
  *
  *
  * @author Sam Reid (PhET Interactive Simulations)
