383fe06bbea1a91701651acbc532999acbcef139
Sam Reid <reids@colorado.edu>
2024-12-03 15:43:28 -0700

    Add deprecation warning, see https://github.com/phetsims/perennial/issues/404

--- js/grunt/tasks/check.ts
+++ js/grunt/tasks/check.ts
@@ -8,6 +8,7 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
+console.log( 'check is deprecated, please use type-check' );
 import { typeCheckPromise } from './type-check.js';
 
 export default typeCheckPromise;
