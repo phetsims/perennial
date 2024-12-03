21a593faae66f79eaf03260d5471496335222f82
Sam Reid <reids@colorado.edu>
2024-12-03 15:38:55 -0700

    Add check as an alias for type-check, see https://github.com/phetsims/perennial/issues/404

---
+++ js/grunt/tasks/check.ts
@@ -0,0 +1,13 @@
+// Copyright 2024, University of Colorado Boulder
+/**
+ * Alias for the type-check task.
+ *
+ * @deprecated, please use type-check
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import { typeCheckPromise } from './type-check.js';
+
+export default typeCheckPromise;
