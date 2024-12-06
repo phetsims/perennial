2bc2ab56ce5309e5a67716f5b925b33f50eb5167
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-06 11:55:51 -0700

    remove TODO, https://github.com/phetsims/chipper/issues/1461

--- js/grunt/tasks/clone-missing-repos.ts
+++ js/grunt/tasks/clone-missing-repos.ts
@@ -3,8 +3,6 @@
 /**
  * Clones any repos not currently checked out in the code base.
  * @author Michael Kauzmann (PhET Interactive Simulations)
- *
- * TODO: ASK DEVS: Delete this grunt task, SR MK think it doesn't belong in formal API (it should just be part of a full thing), https://github.com/phetsims/chipper/issues/1461
  */
 
 import cloneMissingRepos from '../../common/cloneMissingRepos.js';
