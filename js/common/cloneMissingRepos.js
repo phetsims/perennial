e149af740afd126c4b5f429f89c14ebbdec647cd
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-12-10 15:22:11 -0700

    jsdoc, https://github.com/phetsims/perennial/issues/422

--- js/common/cloneMissingRepos.js
+++ js/common/cloneMissingRepos.js
@@ -15,7 +15,7 @@ const execute = require( './execute' ).default;
 /**
  * Clones missing repositories
  * @public
- *
+ * @param [omitPrivate]
  * @returns {Promise.<string>} - The names of the repos cloned
  */
 module.exports = async omitPrivate => {
