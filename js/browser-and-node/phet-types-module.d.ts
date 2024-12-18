c9df20f6b41589c7883b75b957e99c85e7012466
Jesse <jesse.greenberg@colorado.edu>
2024-12-18 10:25:38 -0500

    move fluent type deffinitions to phet-types-module.d.ts, see https://github.com/phetsims/joist/issues/994

--- js/browser-and-node/phet-types-module.d.ts
+++ js/browser-and-node/phet-types-module.d.ts
@@ -8,6 +8,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import type { FluentBundle, FluentResource } from '@types/fluent';
 import * as lodash from 'lodash';
 
 declare global {
@@ -15,4 +16,14 @@ declare global {
   // Specify the correct type for lodash as a global object to work around the TS2686 warning in WebStorm/IntelliJ
   // See https://github.com/phetsims/chipper/issues/1402
   const _: typeof lodash;
+
+  // TODO: Move Fluent to chipper, once we import Fluent from node_modules
+  const Fluent: {
+    FluentBundle: FluentBundle;
+    FluentResource: FluentResource;
+  }
+  namespace Fluent {
+    declare type FluentBundle = FluentBundle;
+    declare type FluentResource = FluentResource;
+  }
 }
