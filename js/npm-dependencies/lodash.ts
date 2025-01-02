90bf114d39d5b1fb51ea0372937129177381159d
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-02 15:38:55 -0700

    remove TODOs, https://github.com/phetsims/chipper/issues/1523

--- js/npm-dependencies/lodash.ts
+++ js/npm-dependencies/lodash.ts
@@ -3,9 +3,6 @@
 /**
  * To be used when trying to import lodash from other repos. This way we don't propagate the devDependency: "@types/lodash"
  * @author Michael Kauzmann (PhET Interactive Simulations)
- *
- * TODO: SR: Let's probably just use the sherpa one, and delete this one.
- * TODO: MK: Why? Seems best to use npm modules in Nodejs code in chipper/perennial, and keep perennial dependency-less, see https://github.com/phetsims/chipper/issues/1523
  */
 
 import lodash from 'lodash';
