cb6636f9909dd2b8fa44ab2001674d4e27085b1f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-03 14:32:27 -0700

    simName bug fix, https://github.com/phetsims/perennial/issues/426

--- js/grunt/deployImages.js
+++ js/grunt/deployImages.js
@@ -31,7 +31,7 @@ const deployImages = async function( { branch, brands, simulation } ) {
     requestObject.email = buildLocal.buildServerNotifyEmail;
   }
   if ( simulation ) {
-    requestObject.simulation = simulation;
+    requestObject.simName = simulation;
     try {
       const metadataResponse = await axios.get( `https://phet.colorado.edu/services/metadata/1.2/simulations?format=json&summary&locale=en&type=html&simulation=${simulation}` );
       if ( metadataResponse.data && metadataResponse.data?.projects?.[ 0 ]?.version?.string ) {
