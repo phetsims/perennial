026cc129f3e1f30004ce4e2fcca9a60f65760548
Sam Reid <reids@colorado.edu>
2025-01-09 14:12:02 -0700

    Simplify clone, see https://github.com/phetsims/chipper/issues/1345

--- bin/v1/init.sh
+++ bin/v1/init.sh
@@ -6,14 +6,47 @@ set -e
 # Install root NPM dependencies
 npm install
 
-# Clone missing repositories
-./bin/clone-missing-repos.sh
+cd ..
 
-# Checkout the desired branch TODO: Change to phetsims-v1
-./bin/checkout-branch.sh main
+# Function to clone repositories only if they don't exist
+clone_if_missing() {
+    local repo_name=$1
+    local target_dir=${2:-$repo_name}  # Default to repo_name if target_dir is not provided
+    local repo_url="https://github.com/phetsims/${repo_name}.git"
+
+    if [ -d "$target_dir" ]; then
+        echo "Repository '$target_dir' already exists. Skipping clone."
+    else
+        echo "Cloning '$repo_url' into '$target_dir'..."
+        # TODO: Change branch to phetsims-v1
+        git clone "$repo_url" -b main --single-branch "$target_dir"
+    fi
+}
+
+# Clone missing repositories
+clone_if_missing "least-squares-regression"
+clone_if_missing "chipper"
+clone_if_missing "phet-core"
+clone_if_missing "perennial" "perennial-alias"
+clone_if_missing "brand"
+clone_if_missing "phetmarks"
+clone_if_missing "sherpa"
+clone_if_missing "query-string-machine"
+clone_if_missing "blackbody-spectrum"
+clone_if_missing "joist"
+clone_if_missing "tambo"
+clone_if_missing "sun"
+clone_if_missing "dot"
+clone_if_missing "tandem"
+clone_if_missing "twixt"
+clone_if_missing "scenery-phet"
+clone_if_missing "phetcommon"
+clone_if_missing "scenery"
+clone_if_missing "utterance-queue"
+clone_if_missing "kite"
 
 # Install NPM dependencies in 'perennial-alias'
-cd ../perennial-alias
+cd ./perennial-alias
 npm install
 
 # Install NPM dependencies in 'chipper'
