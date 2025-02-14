a5850d93562635d68140262cb27ff4ad687133b4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-02-13 18:02:09 -0700

    run pull-all.sh/js and status.sh using grunt sync, https://github.com/phetsims/perennial/issues/405

--- bin/pull-all.sh
+++ bin/pull-all.sh
@@ -2,50 +2,14 @@
 #====================================================================================================
 #
 # Does a 'git pull --rebase' on every active repo.
-# Use -p option to handle each repository in parallel.
 #
 # Author: Jonathan Olson
 #
 #====================================================================================================
 
-pullCommand="git pull --rebase"
-parallel="false"
 
-# cd to the directory where your git repositories live
 binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-workingDir=${binDir}/../..
+workingDir=${binDir}/../
 cd ${workingDir}
 
-# parse command line options
-while getopts ":p" opt; do
-  case ${opt} in
-    p)
-      parallel="true"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit
-      ;;
-  esac
-done
-
-# pull each repository
-for repo in `cat perennial/data/active-repos | xargs | tr -d '\r'`
-do
-  if [ -d "${repo}" ]; then
-    echo ${repo}
-    cd ${repo} > /dev/null
-    if [ ${parallel} == "true" ]; then
-      # run in the background
-      ${pullCommand} &
-    else
-      ${pullCommand}
-    fi
-    cd ..
-  fi
-done
-
-if [ ${parallel} == "true" ]; then
-  # wait for all background tasks to complete
-  wait
-fi
+bin/sage run js/grunt/tasks/sync.ts --npmUpdate=false --status=false --checkoutMain=false $@
--- js/scripts/pull-all.js
+++ js/scripts/pull-all.js
@@ -1,16 +1,9 @@
 // Copyright 2021, University of Colorado Boulder
 
-const execute = require( '../common/execute' ).default;
-const fs = require( 'fs' );
-const path = require( 'path' );
-const chunkDelayed = require( '../common/util/chunkDelayed' ).default;
-
-// Don't use getActiveRepos() since it cannot be run from the root
-const contents = fs.readFileSync( `${__dirname}/../../data/active-repos`, 'utf8' ).trim();
-const repos = contents.split( '\n' ).map( sim => sim.trim() );
-
 /**
- * Pulls all repos (with rebase)
+ * Pulls all repos (with rebase).
+ *
+ * Deprecated, use grunt/tasks/sync.ts instead.
  *
  * USAGE:
  * cd ${root containing all repos}
@@ -19,41 +12,17 @@ const repos = contents.split( '\n' ).map( sim => sim.trim() );
  * cd perennial
  * sage run js/scripts/pull-all.js
  *
- * OPTIONS:
- * --batches=N - (1) by default, running all pulls in parallel. Specify this to separate into N different chunks where
- * each next one is delayed before creating child processes. This helps to solve the OpenSSL SSL_ERROR_SYSCALL bug in https://github.com/phetsims/perennial/issues/361
- *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 ( async () => {
+  console.log( 'DEPRECATED: pull-all.sh is deprecated, please use `sage run js/grunt/tasks/sync.ts --status=false --npmUpdate=false --checkoutMain=false`' );
 
-  const batchesMatch = process.argv.join( ' ' ).match( /--batches=(\d+)/ );
-  const batches = batchesMatch ? batchesMatch[ 1 ] : 1;
-  const results = await chunkDelayed( repos, repo => {
-    return execute( 'git', [ 'pull', '--rebase' ], path.join( __dirname, `../../../${repo}` ), {
+  require( 'process' ).argv.push(
+    '--npmUpdate=false',
+    '--status=false',
+    '--checkoutMain=false'
+  );
 
-      // resolve errors so Promise.all doesn't fail on first repo that cannot pull/rebase
-      errors: 'resolve'
-    } );
-  }, { chunkSize: repos.length / batches } );
-
-  // Report results
-  for ( let i = 0; i < results.length; i++ ) {
-    const repo = repos[ i ];
-    const result = results[ i ];
-
-    // DUPLICATION ALERT: these hard coded strings are also listed in sync.parsePullResult(), please change both cases.
-    if ( result.code === 0 && result.stderr === '' && ( result.stdout === 'Already up to date.\nCurrent branch main is up to date.\n' ||
-                                                        result.stdout === 'Already up to date.\n' ||
-                                                        result.stdout === 'Current branch main is up to date.\n' ) ) {
-      // nothing to do
-    }
-    else {
-      console.log( '##', repo );
-      result.stdout.trim().length > 0 && console.log( result.stdout );
-      result.stderr.trim().length > 0 && console.log( result.stderr );
-      result.error && console.log( result.error );
-    }
-  }
+  require( '../grunt/tasks/sync' );
 } )();
