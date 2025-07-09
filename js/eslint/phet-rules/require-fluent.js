e86229c0b09d1ae758a17e1266ed062a1ec3140e
Jesse <jesse.greenberg@colorado.edu>
2025-07-08 22:02:57 -0400

    get the repo name to look for the strings module, see #1609

--- js/eslint/phet-rules/require-fluent.js
+++ js/eslint/phet-rules/require-fluent.js
@@ -7,8 +7,30 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
+// Convert a dash separated repository name to PascalCase.
+// For example, 'membrane-transport' becomes 'MembraneTransport'.
+function toPascalCase( repoName ) {
+
+  // Catches any character after a dash and replaces it with the uppercase version of that character.
+  const camel = repoName.replace( /-(.)/g, ( _, group1 ) => group1.toUpperCase() );
+
+  // Capitalize the first letter to convert to PascalCase.
+  return camel.charAt( 0 ).toUpperCase() + camel.slice( 1 );
+}
+
 module.exports = {
   create: context => {
+
+    // Get the full file path from ESLint context, then extract the directory name
+    // immediately before '/js/'. For example, if filename is
+    // '/repos/membrane-transport/js/someFile.js', repoNameMatch[1] will be 'membrane-transport'
+    const filename = context.getFilename();
+    const repoNameMatch = filename.match( /[\\/]([^\\/]+)[\\/]js[\\/]/ );
+
+    // The repository may be null if linting a file that is not under the js directory,
+    // such as a Gruntile or config file. Its OK to skip those.
+    const repoName = repoNameMatch ? repoNameMatch[ 1 ] : null;
+    const stringsModuleName = repoName ? `${toPascalCase( repoName )}Strings` : null;
 
     /**
      * Report the given node and (optionally) replace it.
@@ -27,16 +49,16 @@ module.exports = {
 
     return {
 
-      // import MembraneTransportStrings from '…';
+      // import {{Repo}}Strings from '…';
       ImportDefaultSpecifier( node ) {
-        if ( node.local.name === 'MembraneTransportStrings' ) {
+        if ( node.local.name === stringsModuleName ) {
           report( node.local );
         }
       },
 
       // Any runtime identifier usage
       Identifier( node ) {
-        if ( node.name === 'MembraneTransportStrings' ) {
+        if ( node.name === stringsModuleName ) {
           report( node );
         }
       }
