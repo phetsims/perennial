c636b2096ece7a48191444f8ac48bd5bbc399e41
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-08-05 12:25:22 -0600

    fix file extension, https://github.com/phetsims/chipper/issues/1376

--- bin/daily-grunt-work.sh
+++ bin/daily-grunt-work.sh
@@ -124,7 +124,7 @@ cd ..
 
 logWithStderr "TASK - UPDATE LOCALE INFO FILES"
 cd chipper/js/data || exit
-../../../perennial/bin/sage run updateLocaleInfo.js
+../../../perennial/bin/sage run updateLocaleInfo.ts
 git commit -am "Update locale info module.exports from daily grunt work" --no-verify
 git push
 cd ../../..
