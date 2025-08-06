025117d425d17be82e930339a67a5a7cffccd53a
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-08-06 15:09:33 -0600

    pull all branches for daily grunt work, https://github.com/phetsims/chipper/issues/1376

--- bin/daily-grunt-work.sh
+++ bin/daily-grunt-work.sh
@@ -41,7 +41,7 @@ npm prune && npm update
 grunt checkout-main-all
 cd ..
 
-perennial/bin/sage run perennial/js/grunt/tasks/sync --npmUpdate=false
+perennial/bin/sage run perennial/js/grunt/tasks/sync --npmUpdate=false --allBranches
 perennial/bin/sage run perennial/js/scripts/push-all
 
 perennial/bin/for-each.sh perennial/data/active-repos "npm prune && npm update"
