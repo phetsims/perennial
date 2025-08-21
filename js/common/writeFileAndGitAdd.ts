8ca3292101035ec6378930da0d4efa780a86e069
Sam Reid <reids@colorado.edu>
2025-08-21 15:46:59 -0600

    Convert to TypeScript, see https://github.com/phetsims/chipper/issues/1624

--- js/common/writeFileAndGitAdd.ts
+++ js/common/writeFileAndGitAdd.ts
@@ -3,34 +3,46 @@
 /**
  * Writes a file with grunt and adds it to git.
  *
+ * TODO: Rename preserving history, see https://github.com/phetsims/chipper/issues/1624
+ *
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-const gitAdd = require( './gitAdd' );
-const gitIsClean = require( './gitIsClean' );
-const grunt = require( 'grunt' );
-const isGitRepo = require( './isGitRepo' );
+import fs from 'fs';
+import grunt from '../npm-dependencies/grunt.js';
+import fixEOL from './fixEOL.js';
+import gitAdd from './gitAdd.js';
+import gitIsClean from './gitIsClean.js';
+import isGitRepo from './isGitRepo.js';
 
 /**
- * @public
- *
- * @param {string} repo - The repository name
- * @param {string} filePath - File name and potentially path relative to the repo
- * @param {string} content - The content of the file as a string
- * @returns {Promise.<string>} - Stdout
+ * @param repo - The repository name
+ * @param filePath - File name and potentially path relative to the repo
+ * @param content - The content of the file as a string
+ * @param [skipWorkIfUnchanged=false] - If true, the file will not be written and gitAdd will be skipped if the content is identical to the existing file.
+ * @returns - stdout
  * @rejects {ExecuteError}
  */
-module.exports = async function( repo, filePath, content ) {
-  const outputFile = `../${repo}/${filePath}`;
-  grunt.file.write( outputFile, content );
+export default async function( repo: string, filePath: string, content: string ): Promise<void> {
 
-  if ( await isGitRepo( repo ) ) {
-    const fileClean = await gitIsClean( repo, filePath );
-    if ( !fileClean ) {
-      await gitAdd( repo, filePath );
+  const fixedContent = fixEOL( content );
+
+  // TODO: https://github.com/phetsims/chipper/issues/1624 double check on windows after push
+  const fromDiskContent = fixEOL( fs.readFileSync( `../${repo}/${filePath}`, 'utf8' ) );
+
+  if ( fromDiskContent.trim() !== fixedContent.trim() ) {
+
+    const outputFile = `../${repo}/${filePath}`;
+    grunt.file.write( outputFile, fixedContent );
+
+    if ( await isGitRepo( repo ) ) {
+      const fileClean = await gitIsClean( repo, filePath );
+      if ( !fileClean ) {
+        await gitAdd( repo, filePath );
+      }
+    }
+    else {
+      console.warn( `${repo} is not a git repository. Skipping gitAdd.` );
     }
   }
-  else {
-    console.warn( `${repo} is not a git repository. Skipping gitAdd.` );
-  }
-};
+}
