cb250d81f4724e1033278af9473fc7c59342068b
Sam Reid <reids@colorado.edu>
2025-09-12 16:06:50 -0600

    Add console.log, see https://github.com/phetsims/perennial/issues/459

--- js/grunt/tasks/maintenance.ts
+++ js/grunt/tasks/maintenance.ts
@@ -7,4 +7,7 @@
 
 import Maintenance from '../../common/Maintenance.js';
 
+console.log( 'Starting maintenance REPL...' );
+console.log( 'NOTE: We would like to work on the following issues in coordination with the next maintenance release:\n* https://github.com/phetsims/perennial/issues/460\n* https://github.com/phetsims/perennial/issues/461 ' );
+
 ( async () => Maintenance.startREPL() )();
