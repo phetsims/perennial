02854d4764c4ec3b4296060f6338e7db3f0c5711
Jesse <jesse.greenberg@colorado.edu>
2026-04-03 11:45:56 -0400

    remove link to issue that has been closed

--- js/grunt/tasks/maintenance.ts
+++ js/grunt/tasks/maintenance.ts
@@ -8,6 +8,6 @@
 import Maintenance from '../../common/Maintenance.js';
 
 console.log( 'Starting maintenance REPL...' );
-console.log( 'NOTE: We would like to work on the following issues in coordination with the next maintenance release:\n* https://github.com/phetsims/perennial/issues/460\n* https://github.com/phetsims/perennial/issues/461 ' );
+console.log( 'NOTE: We would like to work on the following issues in coordination with the next maintenance release:\n* https://github.com/phetsims/perennial/issues/460\n*' );
 
 ( async () => Maintenance.startREPL() )();
